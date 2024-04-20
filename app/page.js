99207afd4fe5081245bf5698ae43bdf2f0b6d946
rohitsingh93300 <rohitsingh93300@gmail.com>
2024-04-20 20:34:38 +0530

    header page

--- app/page.js
+++ app/page.js
@@ -4,8 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return (
     <div>
-      <h1>Hello world</h1>
-    <Button>Subscribe</Button>
+     Home
     </div>
   );
 }
